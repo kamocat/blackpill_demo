43333899c8ece1a37451d9bd66f1e5ba5d777b93
Marshal <kamocat@gmail.com>
2022-06-30 13:43:21 -0700

    Accepts lower frequencies, but has some trouble below 40kHz

--- src/main.cpp
+++ src/main.cpp
@@ -7,31 +7,64 @@
 
 Adafruit_SI5351 clk;
 
-void set_freq(double hz){
-	double pll = 900E6;
-	double div = pll / hz;
+int log2(unsigned int x){
+	int i = 0;
+	while(x/=2){++i;}
+	return i;
+}
+
+err_t set_freq(double hz){
+	double pll = 600E6;
+	double ratio = pll / hz;
+	int div = ratio / 1024;
+	div = log2(div);
+	ratio /= 1<<div;
 	int a,b,c;
-	a = div;
-	div -= a;
+	a = ratio;
+	ratio -= a;
 	c = 1<<18;
 	b = div * c;
-	clk.setupMultisynth(0, SI5351_PLL_A, a, b, c);
+	clk.setupRdiv(0, si5351RDiv_t(div));
+	ASSERT_STATUS(clk.setupMultisynth(0, SI5351_PLL_A, a, b, c));
+	return ERROR_NONE;
 }
-	
+
+void print_error(err_t e){
+	switch(e){
+		case ERROR_NONE:
+			Serial.println("OK");
+			break;
+		case ERROR_OPERATIONTIMEDOUT:
+			Serial.print("Timeout");
+			break;
+		case ERROR_INVALIDPARAMETER:
+			Serial.print("Bad parameter");
+			break;
+		case ERROR_UNEXPECTEDVALUE:
+			Serial.print("Unexpected value");
+			break;
+		default:
+			Serial.print("Other error ");
+			Serial.print(e, HEX);
+			break;
+	}
+}
 
 void setup()
 {
 	Serial.begin(115200);
 	clk.begin();
-	clk.setupPLLInt(SI5351_PLL_A, 36); // 900MHz
+	clk.setupPLLInt(SI5351_PLL_A, 24); // 600MHz
 }
 
-double i = 1E6;
 void loop()
 {
-	set_freq(i);
-	clk.enableOutputs(true);
-	Serial.println(i);
-	delay(1000);
-	i += 1000;
+	double hz = Serial.parseFloat();
+	if( hz > 0. ){
+		Serial.print(hz);
+		Serial.print(" MHz... ");
+		err_t e = set_freq(hz * 1e6);
+		print_error(e);
+		clk.enableOutputs(true);
+	}
 }
